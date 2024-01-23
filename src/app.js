9954e486928be12ee6172cfc64b2781a76ffbc94
Alex Marin Mendez <alexmarinmendez@gmail.com>
2024-01-22 19:09:26 -0500

    feat: add payments router

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 import express from 'express'
+import router from './routers/payments.router.js'
 
 const app = express()
 
 app.get('/', (req, res) => res.json({ status: 'ok' }))
+app.use('/pay', router)
 
 app.listen(8080, () => console.log('Server Up!'))
